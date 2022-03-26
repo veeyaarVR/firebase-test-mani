43ef0cbe7fb811788af8548801442ef7f9f594cf
veeyaarVR <vigneshmarimuthu2302@gmail.com>
2022-03-26 11:27:17 +0530

    reduced version to 23. copy to clipboard feature

--- app/src/main/java/com/example/notifytest/MainActivity.kt
+++ app/src/main/java/com/example/notifytest/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.notifytest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 
+
 class MainActivity : AppCompatActivity() {
 
-    lateinit var txtView: TextView
+    private lateinit var txtView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,10 @@ class MainActivity : AppCompatActivity() {
             // Get new FCM registration token
             val token = task.result
             txtView.text = token
+            val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("Copied", token)
+            clipboardManager.setPrimaryClip(clip)
+            Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show()
             Log.w("Firebase", "token is : $token")
         })
     }
